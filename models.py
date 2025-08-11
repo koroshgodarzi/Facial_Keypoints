d0b051f849a3075b678d3ea5c3a154b5e05b6288
Korosh <koroshgodarzi73@yahoo.com>
2025-08-11 17:14:00 +0330

    Add models with ELU activation function

--- models.py
+++ models.py
@@ -164,6 +164,57 @@ class Net1(nn.Module):
         return x
 
 
+class Net1_ELU(nn.Module):
+
+    def __init__(self):
+        super(Net1_ELU, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        output_size = self._output_size([self.conv1, self.pool, self.conv2, self.pool], 224)
+
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(64*output_size*output_size, 136)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+    def _output_size(self, layers, input_size):
+        """
+        Compute output size after input going through convolution and pooling layers
+        """
+        output_size = input_size
+        for layer in layers:
+            if isinstance(layer, nn.Conv2d):
+                output_size = (output_size + 2*layer.padding[0] - layer.kernel_size[0] + 1)
+            elif isinstance(layer, nn.MaxPool2d):
+                output_size = (output_size + 2*layer.padding - layer.kernel_size)/layer.stride + 1
+        return int(output_size)
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.ELU(self.conv1(x)))
+        x = self.pool(F.ELU(self.conv2(x)))
+
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
+
 
 class Net2(nn.Module):
 
@@ -272,6 +323,66 @@ class Net3(nn.Module):
         x = self.pool(F.relu(self.conv4(F.relu(self.conv3(x)))))
         x = self.pool(F.relu(self.conv6(F.relu(self.conv5(x)))))
         x = self.pool(F.relu(self.conv8(F.relu(self.conv7(x)))))
+
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
+
+
+class Net3_ELU(nn.Module):
+
+    def __init__(self, conv_layer_num=3):
+        super(Net3_ELU, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 16, 3, padding=1)
+        self.conv3 =  nn.Conv2d(16, 64, 3, padding=1)
+        self.conv4 =  nn.Conv2d(64, 32, 3, padding=1)
+        self.conv5 =  nn.Conv2d(32, 128, 3, padding=1)
+        self.conv6 =  nn.Conv2d(128, 64, 3, padding=1)
+        self.conv7 =  nn.Conv2d(64, 512, 3, padding=1)
+        self.conv8 =  nn.Conv2d(512, 256, 3, padding=1)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        output_size = self._output_size([self.conv1, self.pool, self.conv2, self.pool, self.conv3, self.pool, self.conv4, self.pool], 224)
+
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(256*output_size*output_size, 136)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+    def _output_size(self, layers, input_size):
+        """
+        Compute output size after input going through convolution and pooling layers
+        """
+        output_size = input_size
+        for layer in layers:
+            if isinstance(layer, nn.Conv2d):
+                output_size = (output_size + 2*layer.padding[0] - layer.kernel_size[0] + 1)
+            elif isinstance(layer, nn.MaxPool2d):
+                output_size = (output_size + 2*layer.padding - layer.kernel_size)/layer.stride + 1
+        return int(output_size)
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.ELU(self.conv2(F.ELU(self.conv1(x)))))
+        x = self.pool(F.ELU(self.conv4(F.ELU(self.conv3(x)))))
+        x = self.pool(F.ELU(self.conv6(F.ELU(self.conv5(x)))))
+        x = self.pool(F.ELU(self.conv8(F.ELU(self.conv7(x)))))
 
         x = x.view(x.size(0), -1)
         x = self.dropout(x)
