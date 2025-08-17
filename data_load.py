5cd9199e7566f0822c76892fd2509cfb0d096941
Korosh <koroshgodarzi73@yahoo.com>
2025-08-17 18:33:33 +0330

    Removing per keypoint

--- data_load.py
+++ data_load.py
@@ -11,7 +11,7 @@ import cv2
 class FacialKeypointsDataset(Dataset):
     """Face Landmarks dataset."""
 
-    def __init__(self, csv_file, root_dir, transform=None, per_key_point=False, key_point=0):
+    def __init__(self, csv_file, root_dir, transform=None):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -22,8 +22,7 @@ class FacialKeypointsDataset(Dataset):
         self.key_pts_frame = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
-        self.per_key_point = per_key_point
-        self.key_point = key_point
+
 
     def __len__(self):
         return len(self.key_pts_frame)
@@ -40,8 +39,7 @@ class FacialKeypointsDataset(Dataset):
         
         key_pts = self.key_pts_frame.iloc[idx, 1:].values
         key_pts = key_pts.astype('float').reshape(-1, 2)
-        if per_key_point:
-            key_pts = key_pts[key_point, :]
+
         sample = {'image': image, 'keypoints': key_pts}
 
         if self.transform:
